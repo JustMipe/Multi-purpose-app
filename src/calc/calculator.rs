2ef58ddd84c5c89d8b6c3f2c13a50e60d5419b30
Miroslav Pecenka <pecenka.miroslav@gmail.com>
2025-05-12 19:18:51 +0200

    added: function with own loop

---
+++ src/calc/calculator.rs
@@ -0,0 +1,17 @@
+use crate::utils::clear;
+
+/// Calling calculator
+pub fn calculator() {
+    loop {
+        clear();
+        println!("Welcome in Calculator!");
+        println!("\n1. quit");
+        let mut input: String = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "1" => break,
+            _ => break,
+        }
+    }
+}
