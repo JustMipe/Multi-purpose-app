9fb6e41353f907fe06fe26610b016f27c52e6e97
Miroslav Pecenka <pecenka.miroslav@gmail.com>
2025-05-12 19:19:26 +0200

    added: main menu

---
+++ src/ui/menu.rs
@@ -0,0 +1,8 @@
+///Show menu
+pub fn main_menu() {
+    println!("=====================");
+    println!("  Multi-purpose app");
+    println!("=====================");
+    println!("1. Calculator");
+    println!("2. Quit");
+}
